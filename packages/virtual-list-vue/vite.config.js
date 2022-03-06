089b3ffbf09d174e4a1258cf8f2ac056dc38dc52
phphe <phphe@outlook.com>
2022-03-06 14:38:06 +0800

    feat(iife): rename output name to vtlistVue
    affects: example-iife, @vtlist/vue

--- packages/virtual-list-vue/vite.config.js
+++ packages/virtual-list-vue/vite.config.js
@@ -17,7 +17,7 @@ export const globals = {
   "helper-js": "helperJs",
 };
 
-export const name = "vitualListVue"; // for umd, iife
+export const name = "vtlistVue"; // for umd, iife
 
 // https://vitejs.dev/config/
 export default defineConfig({
