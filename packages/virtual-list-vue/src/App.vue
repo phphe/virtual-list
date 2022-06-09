7cdd518e076206f0b6b5f631eaab7dfddfa373d5
phphe <phphe@outlook.com>
2022-06-09 10:10:01 +0800

    docs(example): iife example
    affects: example-iife, @virtual-list/vue

--- packages/virtual-list-vue/src/App.vue
+++ packages/virtual-list-vue/src/App.vue
@@ -1,16 +1,15 @@
 <script lang="ts">
 import vList from "./components/VirtualList.vue";
 import { reactive, defineComponent, onMounted } from "vue";
-import * as hp from "helper-js";
 
 export default defineComponent({
   components: { "v-list": vList },
   data() {
     return {
-      items: new Array(1000).fill(1).map((v) => ({
-        text: hp.randString(3),
-        lineHeight: 20 + hp.randInt(1, 10) + "px",
-        width: 100 + hp.randInt(1, 30) + "px",
+      items: new Array(1000).fill(1).map((v, i) => ({
+        text: "Item " + i,
+        lineHeight: 20 + (i % 20) + "px",
+        width: 100 + (i % 30) + "px",
       })),
     };
   },
@@ -21,6 +20,7 @@ export default defineComponent({
 <template>
   <div>
     <!-- horizontal -->
+    <h1>horizontal</h1>
     <v-list
       ref="list"
       :items="items"
@@ -34,29 +34,84 @@ export default defineComponent({
         </h2>
       </template>
     </v-list>
-    <!-- vertical -->
-    <v-list ref="list" :items="items" :first-render="10" style="height: 600px">
-      <template #default="{ item, index }">
-        <h2
-          style="border: 1px solid #ccc"
-          :style="{ lineHeight: item.lineHeight }"
+    <div style="display: grid; grid-template-columns: 1fr 1fr 1fr; gap: 20px">
+      <div>
+        <!-- vertical -->
+        <h1>vertical</h1>
+        <v-list
+          ref="list"
+          :items="items"
+          :first-render="10"
+          style="height: 600px"
         >
-          ITEM: {{ index }} - {{ item["text"] }}
-          <input />
-        </h2>
-      </template>
-    </v-list>
-    <v-list ref="list" :items="items" :first-render="10" style="height: 600px">
-      <template #default="{ item, index }">
-        <h2
-          style="border: 1px solid #ccc"
-          :style="{ lineHeight: item.lineHeight }"
+          <template #default="{ item, index }">
+            <h2
+              style="border: 1px solid #ccc"
+              :style="{ lineHeight: item.lineHeight }"
+            >
+              ITEM: {{ index }} - {{ item["text"] }}
+            </h2>
+          </template>
+        </v-list>
+      </div>
+      <div>
+        <h1>vertical with key</h1>
+        <v-list
+          ref="list"
+          :items="items"
+          :first-render="10"
+          style="height: 600px"
+          itemKey="index"
         >
-          ITEM: {{ index }} - {{ item["text"] }}
-        </h2>
-      </template>
-    </v-list>
+          <template #default="{ item, index }">
+            <h2
+              style="border: 1px solid #ccc"
+              :style="{ lineHeight: item.lineHeight }"
+            >
+              ITEM: {{ index }} - {{ item["text"] }}
+              <input />
+            </h2>
+          </template>
+        </v-list>
+      </div>
+      <div>
+        <h1>table</h1>
+        <v-list
+          ref="list"
+          :items="items"
+          :first-render="10"
+          style="height: 600px"
+          table
+          class="list-table"
+        >
+          <template #prepend>
+            <thead>
+              <tr>
+                <th>Text</th>
+                <th>Index</th>
+              </tr>
+            </thead>
+          </template>
+          <template #default="{ item, index }">
+            <tr>
+              <td>{{ index }}</td>
+              <td>ITEM: {{ index }} - {{ item["text"] }}</td>
+            </tr>
+          </template>
+        </v-list>
+      </div>
+    </div>
   </div>
 </template>
 
-<style></style>
+<style>
+.list-table table,
+.list-table td,
+.list-table th {
+  border: 1px solid #ccc;
+}
+.list-table table {
+  border-collapse: collapse;
+  width: 100%;
+}
+</style>
