32f4c9524ceb33313096dd5ce2293945be9c8591
phphe <phphe@outlook.com>
2022-08-09 22:19:59 +0800

    docs(docs scripts): fix docs generate script
    affects: docs-site

--- packages/docs-site/scripts/generate.ts
+++ packages/docs-site/scripts/generate.ts
@@ -16,7 +16,7 @@ async function start() {
   const preview = spawn('npx' + (isWin ? '.cmd' : ''), ['vite', 'preview'])
   preview.stdout.on('data', (data) => {
     console.log('step3: ' + data.toString())
-    if (data.toString().includes('Local:')) {
+    if (data.toString().includes('http://')) {
       step3.resolve()
     }
   })
