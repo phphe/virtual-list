12f0c951f487e182d402128483461da4bed7edb7
phphe <phphe@outlook.com>
2023-03-21 22:58:36 +0800

    docs: add a code comment
    affects: @virtual-list/vue

--- packages/virtual-list-vue/src/components/VirtualList.vue
+++ packages/virtual-list-vue/src/components/VirtualList.vue
@@ -22,6 +22,8 @@
 </template>
 
 <script lang="ts">
+// 如果遇到滚动不流畅的情况，不用处理，因为Dev tool造成的。
+// If the scrolling is not smooth, do not deal with it, because it is caused by the Dev tool.
 import {
   ref,
   computed,
